f652fead894d89576031de59aa66a2d696687e3b
Adamizgoi <adamizgoi97@gmail.com>
2022-07-15 01:04:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 7421; //стоимость авиабилета
+        int one_mile = 20; // кол-во рублей, за которые начисляют 1 милю
+        boolean earn = cost > one_mile; //стоимость билета должна превышать 20р для начисления миль
+
+        if(earn) {
+            int result = cost / one_mile;
+            System.out.println("Начислено миль:");
+            System.out.println(result);
+        } else {
+            System.out.println("Для начисления миль стоимость билета должна превышать 20р");
+        }
+    }
+}
