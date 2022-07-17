cf3fcc87352eef48fcd193d5acd41467713cbf5d
Adamizgoi <adamizgoi97@gmail.com>
2022-07-17 13:28:10 +0300

    Refactor code

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int oneMile = 20; // кол-во рублей, за которые начисляют 1 милю
         boolean earn = cost > oneMile; //стоимость билета должна превышать 20р для начисления миль
 
-        if(earn) {
+        if (earn) {
             int result = cost / oneMile;
             System.out.println("Начислено миль:");
             System.out.println(result);
